ccc3cf9df5db88c368fe667ad7e23f1b357c1034
alaingilbert <alain.gilbert.15@gmail.com>
2013-01-03 19:24:11 -0500

    thermistor example

---
+++ thermistor/thermistor.ino
@@ -0,0 +1,36 @@
+#include <math.h>
+
+#define ThermistorPIN 0                 // Analog Pin 0
+
+float pad = 9850;                       // balance/pad resistor value, set this to
+                                        // the measured resistance of your pad resistor
+float thermr = 10000;                   // thermistor nominal resistance
+
+float Thermistor(int RawADC) {
+  long Resistance;
+  float Temp;  // Dual-Purpose variable to save space.
+
+  Resistance=((1024 * pad / RawADC) - pad);
+  Temp = log(Resistance); // Saving the Log(resistance) so not to calculate  it 4 times later
+  Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
+  Temp = Temp - 273.15;  // Convert Kelvin to Celsius
+  // Uncomment this line for the function to return Fahrenheit instead.
+  //temp = (Temp * 9.0)/ 5.0 + 32.0;                  // Convert to Fahrenheit
+  return Temp;                                      // Return the Temperature
+}
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  float temp;
+  temp = Thermistor(analogRead(ThermistorPIN));       // read ADC and  convert it to Celsius
+  Serial.print("Celsius: ");
+  Serial.print(temp,1);                             // display Celsius
+  //temp = (temp * 9.0)/ 5.0 + 32.0;                  // converts to  Fahrenheit
+  //Serial.print(", Fahrenheit: ");
+  //Serial.print(temp,1);                             // display  Fahrenheit
+  Serial.println("");
+  delay(500);                                      // Delay a bit...
+}
